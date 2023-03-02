bd9b5bfdf89c95e12bde135ce7b4344d3ef727d2
Sam Schoedel <seschoedel@gmail.com>
2023-03-01 21:40:44 -0500

    format examples folder files
    Google style with clang-format

--- c/examples/riccati/main_lqr_track.c
+++ c/examples/riccati/main_lqr_track.c
@@ -1,10 +1,10 @@
 // Check README.md
 // Sources: Lec. 7 Code on double integrator
 // Task: LTI system track a nominal trajectory in MPC style
-//TODO: convert this to test Riccati
+// TODO: convert this to test Riccati
 
-#include "slap/slap.h"
 #include "riccati.h"
+#include "slap/slap.h"
 #include "util.h"
 
 #define H 0.1
@@ -18,14 +18,14 @@ double R_data[NINPUTS * NINPUTS] = {0.1};
 double q_data[NSTATES] = {0., 0.};
 double r_data[NINPUTS] = {0.};
 double A_data[NSTATES * NSTATES] = {1, 0, H, 1};
-double B_data[NSTATES * NINPUTS] = {0.5*H*H, H};
+double B_data[NSTATES * NINPUTS] = {0.5 * H * H, H};
 
 double x0_data[NSTATES] = {2., 0.5};
-double xn_data[NSTATES * NSIM]; // nominal states
-double un_data[NINPUTS * (NSIM - 1)]; // nominal inputs
+double xn_data[NSTATES * NSIM];        // nominal states
+double un_data[NINPUTS * (NSIM - 1)];  // nominal inputs
 
-double Pp_data[NSTATES * (NSTATES + 1) * NHORIZON]; //stores P and p
-double Kd_data[NINPUTS * (NSTATES + 1) * NHORIZON]; //stores K and d 
+double Pp_data[NSTATES * (NSTATES + 1) * NHORIZON];  // stores P and p
+double Kd_data[NINPUTS * (NSTATES + 1) * NHORIZON];  // stores K and d
 
 double x_data[NSTATES * NSIM];
 double u_data[NINPUTS * (NSIM - 1)];
@@ -67,9 +67,8 @@ int main(void) {
   double *xnp = xn_data;
 
   for (int k = 0; k < NSIM; ++k) {
-    // Pointer to each block, then next 
-    if (k < NHORIZON)
-    {
+    // Pointer to each block, then next
+    if (k < NHORIZON) {
       Phist[k] = slap_MatrixFromArray(NSTATES, NSTATES, Pp);
       Pp += NSTATES * NSTATES;
       phist[k] = slap_MatrixFromArray(NSTATES, 1, Pp);
@@ -83,9 +82,8 @@ int main(void) {
     xp += NSTATES;
     xn[k] = slap_MatrixFromArray(NSTATES, 1, xnp);
     xnp += NSTATES;
-    
-    if (k < NSIM - 1)
-    {
+
+    if (k < NSIM - 1) {
       uhist[k] = slap_MatrixFromArray(NINPUTS, 1, up);
       up += NINPUTS;
       un[k] = slap_MatrixFromArray(NINPUTS, 1, unp);
@@ -94,24 +92,23 @@ int main(void) {
   }
   // End of initializing memory and variables
 
-  // Temporary matrix for underlying calculation 
+  // Temporary matrix for underlying calculation
   Matrix S = slap_NewMatrixZeros(NSTATES + NINPUTS, NSTATES + NINPUTS + 1);
-  slap_MatrixCopy(xhist[0], x0);  
+  slap_MatrixCopy(xhist[0], x0);
 
   // MPC loop
   for (int k = 0; k < NSIM - NHORIZON - 1; ++k) {
-    tiny_LQR_LTI(NHORIZON - 1, A, B, Q, R, q, r, 
-                     Khist, dhist, Phist, phist, S);
+    tiny_LQR_LTI(NHORIZON - 1, A, B, Q, R, q, r, Khist, dhist, Phist, phist, S);
 
-    // Control input: u = uf - d - K*(x - xf) 
-    slap_MatrixAddition(uhist[k], un[k], dhist[0], -1);    // u[k] = un[k] + d[k]
-    slap_MatMulAdd(uhist[k], Khist[0], xhist[k], -1, 1);   // u[k] -= K[k] * x[k]
-    slap_MatMulAdd(uhist[k], Khist[0], xn[k], 1, 1);   // u[k] += K[k] * xn[k]
+    // Control input: u = uf - d - K*(x - xf)
+    slap_MatrixAddition(uhist[k], un[k], dhist[0], -1);   // u[k] = un[k] + d[k]
+    slap_MatMulAdd(uhist[k], Khist[0], xhist[k], -1, 1);  // u[k] -= K[k] * x[k]
+    slap_MatMulAdd(uhist[k], Khist[0], xn[k], 1, 1);  // u[k] += K[k] * xn[k]
 
     // Next state: x = A*x + B*u
     slap_MatMulAdd(xhist[k + 1], A, xhist[k], 1, 0);  // x[k+1] = A * x[k]
     slap_MatMulAdd(xhist[k + 1], B, uhist[k], 1, 1);  // x[k+1] += B * u[k]
-    
+
     printf("ex[%d] = %.4f\n", k, slap_MatrixNormedDifference(xn[k], xhist[k]));
   }
   slap_FreeMatrix(S);

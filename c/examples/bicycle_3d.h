9c6b2b2cc86b0b4fddbedf9e6a2c03ff8d9f3249
xkhainguyen <nxk120199@gmail.com>
2023-04-14 22:16:12 -0400

    up

---
+++ c/examples/bicycle_3d.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <math.h>
+
+#include <slap/slap.h>
+
+//========================================
+// Bicycle model parameters
+// X = [x; y; theta] : x, y, yaw
+// U = [v; delta] : velocity and steering angle
+//========================================
+
+//========================================
+// Codes generated from julia/bicycle_tvlqr
+// Discrete dynamics of bicycle model with predefined model params
+//========================================
+void tiny_Bicycle3dNonlinearDynamics_Raw(sfloat* xn, const sfloat* x,
+                                         const sfloat* u);
+
+void tiny_Bicycle3dNonlinearDynamics(Matrix* xn, const Matrix x,
+                                     const Matrix u);
+
+//========================================
+// Codes generated from julia/bicycle_tvlqr
+// Jacobians of discrete dynamics of bicycle model with predefined model params
+//========================================
+void tiny_Bicycle3dGetJacobianA_Raw(sfloat* A, const sfloat* x,
+                                    const sfloat* u);
+
+void tiny_Bicycle3dGetJacobianB_Raw(sfloat* B, const sfloat* x,
+                                    const sfloat* u);
+
+void tiny_Bicycle3dGetJacobians(Matrix* A, Matrix* B, const Matrix x,
+                                const Matrix u);

5ac5be8b5babe2347cc0f3f0e650f4d87ed9735d
Anoushka Alavilli <anoushkaalavill@anoushkas-mbp-2.wifi.local.cmu.edu>
2023-04-24 17:36:22 -0400

    Add line search and regularization

--- julia/rocket_landing_Zac/tinyMPC.jl
+++ julia/rocket_landing_Zac/tinyMPC.jl
@@ -1,21 +1,21 @@
 # -------------------THIS IS ALL ALTRO-------------------------------
 # This enables goal equality constraints, state and input ineq constraints.
 function stage_cost(p::NamedTuple, x, u, k)
-    dx = x - p.Xref[k]
-    du = u - p.Uref[k]
+    dx = x .- p.Xref[k]
+    du = u .- p.Uref[k]
     return 0.5 * dx' * p.Q * dx + 0.5 * du' * p.R * du
 end
 function term_cost(p::NamedTuple, x)
     dx = x - p.Xref[p.N]
     return 0.5 * dx' * p.Qf * dx
 end
-function stage_cost_expansion(p::NamedTuple, k)
-    dx = -p.Xref[k]
-    du = -p.Uref[k]
-    return p.Q, p.Q * dx, p.R, p.R * du  # Hessian and gradient
+function stage_cost_expansion(p::NamedTuple, x, u, k)
+    dx = x .- p.Xref[k]
+    du = u .- p.Uref[k]
+    return p.Q, p.Q*dx, p.R, p.R*du  # Hessian and gradient
 end
-function term_cost_expansion(p::NamedTuple)
-    dx = -p.Xref[p.N]
+function term_cost_expansion(p::NamedTuple, x)
+    dx = x - p.Xref[p.N] # delta formulation
     return p.Qf, p.Qf * dx
 end
 function conic_cost_expansion(p::NamedTuple, u, λc, ρ, k)
@@ -35,7 +35,7 @@ function backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, ρ, λ, λc)
     ΔJ = 0.0    # expected cost reduction
 
     # terminal cost expansion
-    P[N], p[N] = term_cost_expansion(params)
+    P[N], p[N] = term_cost_expansion(params, X[N])
 
     if (params.ncx > 0)
         # # add AL terms for the state constraint at the final time step
@@ -43,18 +43,21 @@ function backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, ρ, λ, λc)
         mask = eval_mask(μx[N], hxv)
         ∇hx = ineq_con_x_jac(params, X[N])
         # add these into the cost-to-go p and P
-        p[N] += ∇hx' * (μx[N] - ρ * (mask * [params.x_max; -params.x_min]))  # multiplier term (1st)
+        p[N] += ∇hx' * (μx[N] + ρ * (mask * hxv))  # multiplier term (1st)
         P[N] += ρ * ∇hx' * mask * ∇hx                # penalty term (2nd)
     end
 
     if (params.ncg > 0)
         # add AL terms for goal constraint 
+        hxv = X[N] - params.Xref[N]
         ∇hx = diagm(ones(params.nx))
         # add these into the CTG p and P (equality active)
-        p[N] += ∇hx' * (λ - ρ * params.Xref[N])
+        p[N] += ∇hx' * (λ + ρ*hxv)
         P[N] += ρ * ∇hx'∇hx
     end
 
+    Qu_bkwdpass = [zeros(nu) for i=1:N]
+    Quu_bkwdpass = [zeros(nu,nu) for i=1:N]
     # iterate from N-1 to 1 backwards
     for k = (N-1):(-1):1
         # dynamics jacobians (linear) (still give the same A, B, f)
@@ -62,49 +65,66 @@ function backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, ρ, λ, λc)
         B = ForwardDiff.jacobian(_u -> discrete_dynamics(params, X[k], _u, k), U[k])
         f = discrete_dynamics(params, zeros(params.nx), zeros(params.nu), k)
 
-        Sxx, Sx, Suu, Su = stage_cost_expansion(params, k)
-
-        # one-step cost expansion: Q, Q*dx, R, R*du
-        Sx += A' * (P[k+1] * f + p[k+1])
-        Su += B' * (P[k+1] * f + p[k+1])
-        Sxx += A' * (P[k+1]) * A
-        Suu += B' * (P[k+1] + reg * I) * B
-        Sux = B' * (P[k+1]) * A
-
-        if (params.ncu > 0)
+        Sxx, Sx, Suu, Su = stage_cost_expansion(params, X[k], U[k], k)
+        if params.ncu > 0
             # control constraints
             huv = ineq_con_u(params, U[k])  # calculate h(u) constraint
             mask = eval_mask(μ[k], huv)  # choose active
             ∇hu = ineq_con_u_jac(params, U[k])
-            Su += ∇hu' * (μ[k] - ρ * (mask * [params.u_max; -params.u_min])) # add to cost
+            Su += ∇hu' * (μ[k] + ρ * (mask * huv)) # add to cost
             Suu += ρ * ∇hu' * mask * ∇hu
         end
-        if (params.ncx > 0)
+        if params.ncx > 0
             # state constraints
             hxv = ineq_con_x(params, X[k])
             mask = eval_mask(μx[k], hxv)
             ∇hx = ineq_con_x_jac(params, X[k])
-            Sx += ∇hx' * (μx[k] - ρ * (mask * [params.x_max; -params.x_min]))
+            Sx += ∇hx' * (μx[k] + ρ * (mask * hxv))
             Sxx += ρ * ∇hx' * mask * ∇hx
         end
-        if (params.ncu_cone > 0)
+        if params.ncu_cone > 0
             # conic constraints
             Qu, Quu = conic_cost_expansion(params, U[k], λc[k], ρ * cone_scale, k)
             # display(Quu)
             Su += Qu
             Suu += Quu
         end
+        
+        # Q expansion
+        gx = Sx + A'p[k+1]
+        gu = Su + B'*p[k+1]
 
-        F = cholesky(Symmetric(Suu))
-        d[k] = F \ Su
-        K[k] = F \ Sux
+        # one-step cost expansion: Q, Q*dx, R, R*du
+        # Sx += A' * (P[k+1] * f + p[k+1])
+        # Su += B' * (P[k+1] * f + p[k+1])
+
+        # Regularization
+        Sxx_reg = Sxx + A' * (P[k+1] + reg*I) * A
+        Suu_reg = Suu + B' * (P[k+1] + reg*I) * B
+        Sux_reg = B' * (P[k+1] + reg*I) * A
+
+        F = cholesky(Symmetric(Suu_reg))
+        d[k] = F \ gu
+        K[k] = F \ Sux_reg
 
         # Cost-to-go Recurrence (PSD stabilizing version, last term)
-        P[k] = Sxx + K[k]' * Suu * K[k] - K[k]' * Sux - Sux' * K[k]
-        p[k] = Sx + K[k]' * Suu * d[k] - K[k]' * Su - Sux' * d[k]
-    end
+        # P[k] = Sxx + K[k]' * Suu * K[k] - K[k]' * Sux - Sux' * K[k]
+        # p[k] = Sx + K[k]' * Suu * d[k] - K[k]' * Su - Sux' * d[k]
+        P[k] = Sxx + K[k]'*Suu*K[k] + (A-B*K[k])'*P[k+1]*(A-B*K[k])
+        p[k] = Sx - K[k]'*Su + K[k]'*Suu*d[k] + (A - B*K[k])'*(p[k+1] - P[k+1]*B*d[k])
 
-    return 0.0 
+        # # @show P[k+1]
+        # @show gu
+        # @show Sux_reg
+        # @show Suu_reg
+        # return
+
+        Qu_bkwdpass[k] = Su
+        Quu_bkwdpass[k] = Suu
+    end
+    # @show d
+    # @show K
+    return Qu_bkwdpass, Quu_bkwdpass
 end
 function trajectory_AL_cost(params, X, U, μ, μx, ρ, λ, λc)
     # Evaluate merit function
@@ -146,23 +166,55 @@ function trajectory_AL_cost(params, X, U, μ, μx, ρ, λ, λc)
     end
     return J
 end
-function forward_pass!(params, X, U, K, d, ΔJ, Xn, Un, μ, μx, ρ, λ, λc; max_linesearch_iters=20)
+function trajectory_AL_cost_grad(params, X, U, μ, μx, ρ, λ, λc)
+    @show trajectory_AL_cost(params, X, U, μ, μx, ρ, λ, λc)
+    return FiniteDiff.gradient(du -> trajectory_AL_cost(params, X, du, μ, μx, ρ, λ, λc), U)
+end
+function update_reg(reg,α;reg_min=1e-6,reg_max=1e2)
+    if α == 0.0
+        if reg == reg_max
+            error("reached max reg")
+        end
+        return min(reg_max,reg*10)
+    end
+    if α == 1.0
+        return max(reg_min,reg/10)
+    end
+    return reg
+end
+function forward_pass!(params, reg, X, U, K, d, Qu, Quu, ΔJ, Xn, Un, μ, μx, ρ, λ, λc; max_linesearch_iters=20)
     """LQR forward pass
     This !function update its parameters
     """
     N = params.N
+    nx = params.nx
+    nu = params.nu
     # Forward Rollout
     Xn[1] .= X[1]
     J = trajectory_AL_cost(params, X, U, μ, μx, ρ, λ, λc)
-    for k = 1:(N-1)
-        Un[k] = -d[k] - K[k] * Xn[k]
-        Xn[k+1] = discrete_dynamics(params, Xn[k], Un[k], k)
+    Jn = 0.0    
+    α = 1
+    γ = 0.5
+    J = trajectory_AL_cost(params,X,U,μ,μx,ρ,λ,λc)
+    for _ = 1:20 # line search iters
+
+        # Forward Rollout
+        for k = 1:(N-1)
+            Un[k] = U[k] - α*d[k] - K[k]*(Xn[k]-X[k])
+            Xn[k+1] = discrete_dynamics(params,Xn[k],Un[k],k)
+        end
+        Jn = trajectory_AL_cost(params,Xn,Un,μ,μx,ρ,λ,λc)
+
+        # linesearch
+        if Jn < J
+            X .= Xn
+            U .= Un
+            return Jn, J - Jn, α
+        else
+            α *= γ
+        end
     end
-    Jn = trajectory_AL_cost(params, Xn, Un, μ, μx, ρ, λ, λc)
-    ΔJ = J - Jn  # Just experiment, not the right way
-    X .= Xn
-    U .= Un
-    return Jn, ΔJ
+    return Jn, J - Jn, α
 end
 function eval_mask(μv, huv)
     # Extract active inequality constraints
@@ -173,8 +225,7 @@ function eval_mask(μv, huv)
     end
     mask
 end
-function tiny_solve!(params, X, U, P, p, K, d, Xn, Un; atol=1e-3, max_iters=50, max_inner_iters=10, verbose=true, ρ=1, ρ_max=1e8, ϕ=10)
-
+function tiny_solve!(params, X, U, P, p, K, d, Xn, Un; atol=1e-3, max_iters=250, max_inner_iters=10, verbose=true, ρ, ρ_max=1e8, ϕ=10)
     # first check the sizes of everything
     # @assert length(X) == params.N
     # @assert length(U) == params.N-1
@@ -187,33 +238,28 @@ function tiny_solve!(params, X, U, P, p, K, d, Xn, Un; atol=1e-3, max_iters=50, 
         X[i+1] = discrete_dynamics(params, X[i], U[i], i)
     end
 
-    reg_min = 1e-8  # can be zero
+    reg_min = 1e-8
     reg = reg_min
 
     μ = params.μ
     μx = params.μx
     λ = params.λ
     λc = params.λc
-
-    # Solve unconstrained LQR first
-    backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, 0.0, λ, λc)
-    J, ΔJ = forward_pass!(params, X, U, K, d, ΔJ, Xn, Un, μ, μx, 0.0, λ, λc)
-
+    
     # Inner loop to solve unconstrained problem (Riccati)
     for iter = 1:max_iters
-        α = 1.0  # no line-search
-
+        J = 0
+        α = 1
         # Riccati solve
         for i = 1:max_inner_iters
-            backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, ρ, λ, λc)
-            J, ΔJ = forward_pass!(params, X, U, K, d, ΔJ, Xn, Un, μ, μx, ρ, λ, λc)
-            if verbose 
-                @show ΔJ
-            end
-            if 0.0 <= ΔJ <= 1.0  # experiment, not a good one
+            Qu, Quu = backward_pass!(params, X, U, P, p, d, K, reg, μ, μx, ρ, λ, λc)
+            J, ΔJ, α = forward_pass!(params, reg, X, U, K, d, Qu, Quu, ΔJ, Xn, Un, μ, μx, ρ, λ, λc)
+            update_reg(reg, α)
+            
+            if ΔJ < 1e-1  # coarse tolerance
+                @show ForwardDiff.gradient(du -> trajectory_AL_cost(params, X, du, μ, μx, ρ, λ, λc), U)
                 break
             end
-            # reg = reg*10.0  # if needed
         end
         reg = reg_min
 
@@ -226,56 +272,58 @@ function tiny_solve!(params, X, U, P, p, K, d, Xn, Un; atol=1e-3, max_iters=50, 
                 iter, J, ΔJ, 0, α, reg, ρ)
         end
 
-        # AL update and check constraint violation
+        # AL update and  check constraint violation
         convio = 0.0
+        dmax = calc_max_d(d)
+        if (α > 0) & (dmax < atol)
+            if (params.ncu > 0)
+                # control constraints (inequality)
+                for k = 1:N-1
+                    huv = ineq_con_u(params, U[k])
+                    mask = eval_mask(μ[k], huv)
+                    # update dual
+                    convio = max(convio, norm(huv + abs.(huv), Inf))
+                    μ[k] .= max.(0, μ[k] + ρ*mask*huv)
+                end
+            end
+            if (params.ncx > 0)
+                # state constraints (inequality)
+                for k = 1:N
+                    hxv = ineq_con_x(params, X[k])
+                    mask = eval_mask(μx[k], hxv)
+                    # update dual
+                    convio = max(convio, norm(hxv + abs.(hxv), Inf))
+                    μx[k] .= max.(0, μx[k] + ρ*mask*hxv)
+                end
+            end
+            if (params.ncu_cone > 0)
+                # conic constraints 
+                if verbose
+                    print("update cone\n")
+                end
+                for k = 1:N-1
+                    Uc = cone_u(params, U[k])
+                    huc = norm(Uc[1:2]) - Uc[3]
+                    convio = max(convio, norm(huc + abs.(huc), Inf))
+                    # update dual
+                    λc[k] .= projection(λc[k] - Uc * cone_scale * ρ)
+                end
+            end
+            if (params.ncg > 0)
+                # goal constraint (equality)
+                hxv = X[N] - params.Xref[N]
+                λ .-= ρ * hxv
+                convio = max(convio, norm(hxv, Inf))
+            end
 
-        if (params.ncu > 0)
-            # control constraints (inequality)
-            for k = 1:N-1
-                huv = ineq_con_u(params, U[k])
-                mask = eval_mask(μ[k], huv)
-                # update dual
-                convio = max(convio, norm(huv + abs.(huv), Inf))
-                μ[k] .= max.(0, μ[k] - ρ * (mask * [params.u_max; -params.u_min]))
-            end
+            ρ *= ϕ  # update penalty
         end
-        if (params.ncx > 0)
-            # state constraints (inequality)
-            for k = 1:N
-                hxv = ineq_con_x(params, X[k])
-                mask = eval_mask(μx[k], hxv)
-                # update dual
-                convio = max(convio, norm(hxv + abs.(hxv), Inf))
-                μx[k] .= max.(0, μx[k] - ρ * (mask * [params.x_max; -params.x_min]))
-            end
-        end
-        if (params.ncu_cone > 0)
-            # conic constraints 
-            if verbose
-                print("update cone\n")
-            end
-            for k = 1:N-1
-                Uc = cone_u(params, U[k])
-                huc = norm(Uc[1:2]) - Uc[3]
-                convio = max(convio, norm(huc + abs.(huc), Inf))
-                # update dual
-                λc[k] .= projection(λc[k] - Uc * cone_scale * ρ)
-            end
-        end
-        if (params.ncg > 0)
-            # goal constraint (equality)
-            hxv = X[N] - params.Xref[N]
-            λ .-= ρ * params.Xref[N]
-            convio = max(convio, norm(hxv, Inf))
-        end
-
-        ρ *= ϕ  # update penalty
 
         if verbose
             @show convio
         end
         if convio < atol  # if terminal condition with contraint violation
-            # print("Our solver says SUCCESS\n") # @info "success!"
+            print("Our solver says SUCCESS\n")
             return Un[1]
         end
         if ρ > ρ_max
@@ -291,10 +339,10 @@ end
 # Constraints
 # =============================
 function ineq_con_x(p, x)
-    [x - p.x_max; -x + p.x_min]
+    [x .- p.x_max; -x .+ p.x_min]
 end
 function ineq_con_u(p, u)
-    [u - p.u_max; -u + p.u_min]
+    [u .- p.u_max; -u .+ p.u_min]
 end
 function cone_u(p, u)
     [p.A_cone * u; p.c_cone' * u]
@@ -308,8 +356,8 @@ end
 function ineq_con_u_jac(params, u)
     ForwardDiff.jacobian(_u -> ineq_con_u(params, _u), u)  # lazy way
 end
-function ineq_con_x_jac(p, x)
-    ForwardDiff.jacobian(_x -> ineq_con_x(p, _x), x)  # lazy way
+function ineq_con_x_jac(p,x)
+    ForwardDiff.jacobian(_x -> ineq_con_x(p,_x),x) # lazy way
 end
 function projection(x)
     # assumes x is stacked [v; s] such that ||v||₂ ≤ s
@@ -486,4 +534,11 @@ function shift_fill(U::Vector)
         U[k] .= U[k+1]
     end
     U[N] .= U[N-1]
+end
+function calc_max_d(d)
+    dm = 0.0
+    for i = 1:length(d)
+        dm = max(dm,norm(d[i]))
+    end
+    return dm
 end
